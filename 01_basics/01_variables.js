2241d61987b23faeb7d51f275304f21ec34da398
bhagatvir <bhagatvirsinghsandhu@gmail.com>
2025-06-19 15:46:45 +0000

    Start JavaScript basics with intro logs

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+console.log("Hello, world")
+console.table(['Bhagat, Hello']);
+console.log("Ok, so this is my first line of code in JavaScript");
+console.log()
+
+console.log("So, recently I went to a pool and it made me feel uncomfortable")
+console.log("Lol, I feel sick")
+
