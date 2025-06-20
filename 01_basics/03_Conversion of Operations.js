c115e36eadc9358b413e90ee3b5569858ebaf8f7
bhagatvir <bhagatvirsinghsandhu@gmail.com>
2025-06-20 16:37:38 +0000

    Simply corrected a wrong name for the file, which could cause some errors

---
+++ 01_basics/03_Conversion of Operations.js
@@ -0,0 +1,37 @@
+//* Conversion Operations
+//~ String to Number Conversion
+
+// node "01_basics/03_Conversion of Operations.js"
+
+let score = "Bhagatvir"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+ "88" => 88
+ "88abc" => NaN
+ true => 1; false => 0
+*/
+
+//~ Number to String Conversion
+let isLoggedIn = "88bc"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"Bhagatvir" => true
+*/
+
+//~ Number to String Conversion
+let someNumber = 88
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
