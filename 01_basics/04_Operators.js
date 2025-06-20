468e7230be95a3bd2c1cd7075c84a41d7cbc6203
bhagatvir <bhagatvirsinghsandhu@gmail.com>
2025-06-20 16:31:56 +0000

    Added new files for operators and type conversions; removed outdated math comparison file

---
+++ 01_basics/04_Operators.js
@@ -0,0 +1,53 @@
+//* Operators
+
+//? node "01_basics/04_Operators.js"
+
+let value = 3
+let negValue = -value
+console.log();
+
+console.log(value);
+console.log(negValue);
+console.log();
+
+console.log()
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(45 / 3);
+console.log(45 % 3);
+console.log();
+
+let For1 = "Good "
+let For2 = "Morning"
+
+let For3 = For1 + For2
+console.log(For3);
+console.log(typeof For3);
+
+console.log();
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log();
+
+console.log( (3 + 4) * (5 % 3));
+console.log();
+
+console.log(true)
+console.log(true + true);
+console.log(+ true);
+console.log(+ "");
+console.log();
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+console.log();
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
---
+++ 01_basics/05_Comparison.js
@@ -0,0 +1,26 @@
+//* Comparison Operators
+//? node "01_basics/05_Comparison.js"
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log();
+
+console.log("2" > 1);
+console.log("02" > 10);
+console.log();
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log();
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log();
+
+console.log("2" == 2);
+console.log(2 === "2");
