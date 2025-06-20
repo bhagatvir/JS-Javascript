896e54c995c3c8cf87f91e111032ef499ee420fa
bhagatvir <bhagatvirsinghsandhu@gmail.com>
2025-06-20 11:09:25 +0000

    Add JavaScript data types with Gojo variables and Sukuna alerts fun

---
+++ 01_basics/02_Data types.js
@@ -0,0 +1,40 @@
+//* Data types
+
+"use strict"; //~ Treats all code with newer versions
+
+// node "01_basics/02_Data types.js"
+
+/*
+alert can be used in browser and not in node.js, try in school. Example:
+
+alert("📚 Studying detected! Please shut down all fun immediately.");
+alert("💻 You are now connected to the secret NASA mainframe...");
+alert("3 + 3)
+*/ 
+
+console.log("Bhagatvir Singh");
+
+
+let name = "Gojo";
+let age = 27;
+let isLoggedin = true;
+let temperature = null;
+
+/*  number => 32
+bigint => 2 to power 64
+string => ""
+boolean => true/false
+null => standalone value
+undefined => not assigned 
+symbol => unique
+*/
+
+//^ object => complex data structure
+console.log();
+
+console.log(typeof "Sukuna");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 3.14);
+
+
